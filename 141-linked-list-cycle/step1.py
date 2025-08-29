fa91e8ef15925863587919a3e14da3fce6db5ca6
nanae <desperado.kazune@gmail.com>
2025-08-29 17:59:27 +0900

    素直な解

---
+++ 141-linked-list-cycle/step1.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: ListNode | None) -> bool:
+        checked_nodes = set()
+        cur_node = head
+
+        while cur_node is not None:
+            if cur_node in checked_nodes:
+                return True
+            checked_nodes.add(cur_node)
+            cur_node = cur_node.next
+
+        return False
