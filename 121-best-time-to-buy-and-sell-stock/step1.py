6d50634e9d648a86b63b228eed3e6a5a8a588dbd
nanae <desperado.kazune@gmail.com>
2025-10-12 18:07:54 +0900

    add solutions

---
+++ 121-best-time-to-buy-and-sell-stock/step1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = prices[0]
+        for price in prices:
+            max_profit = max(max_profit, price - min_price)
+            min_price = min(min_price, price)
+        return max_profit
